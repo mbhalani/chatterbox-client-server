61d819fa87a1754738bb5e4d0e7074c794ecb5aa
Mukesh B <mbhalani3@gmail.com>
2017-03-10 23:09:15 -0500

    update

--- server/request-handler.js
+++ server/request-handler.js
@@ -2,11 +2,11 @@ console.log('HANDLER: Line 1');
 
 // These headers will allow Cross-Origin Resource Sharing (CORS).
 var header = {
-  'access-control-allow-origin':  '*',
+  'access-control-allow-origin': '*',
   'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
   'access-control-allow-headers': 'content-type, accept',
-  'access-control-max-age':       10, // Seconds.
-  'Content-Type':                 'application/json',
+  'access-control-max-age': 10, // Seconds.
+  'Content-Type': 'application/json',
 };
 
 var requestHandler = function(req, res) {
